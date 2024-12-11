fa209b1cb01e29c2ed963477b542aec66f14010a
vonum <vonum.mk@gmail.com>
2024-12-11 20:35:36 +0100

    Add game helper tests

--- x/checkers/types/genesis_test.go
+++ x/checkers/types/genesis_test.go
@@ -64,3 +64,14 @@ func TestGenesisState_Validate(t *testing.T) {
 		})
 	}
 }
+
+func TestGenesisState_ExpectedInitialNextId(t *testing.T) {
+  require.EqualValues(
+    t,
+    &types.GenesisState{
+      StoredGameList: []types.StoredGame{},
+      SystemInfo: types.SystemInfo{uint64(1)},
+    },
+    types.DefaultGenesis(),
+  )
+}
