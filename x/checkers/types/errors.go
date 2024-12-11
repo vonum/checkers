5e9089e4403a36b29249fa3ded026c5361b3faa3
vonum <vonum.mk@gmail.com>
2024-12-11 18:47:46 +0100

    Add game state helper methods

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,6 +8,9 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrInvalidSigner = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
-	ErrSample        = sdkerrors.Register(ModuleName, 1101, "sample error")
+	ErrInvalidSigner    = sdkerrors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrSample           = sdkerrors.Register(ModuleName, 1101, "sample error")
+  ErrInvalidBlack     = sdkerrors.Register(ModuleName, 1102, "black address is invalid %s")
+  ErrInvalidRed       = sdkerrors.Register(ModuleName, 1103, "red address is invalid %s")
+  ErrGameNotParseable = sdkerrors.Register(ModuleName, 1104, "game cannot be parsed")
 )
